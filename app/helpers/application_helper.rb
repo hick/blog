f74ea759b1108539c944309638704d74bd255372
hick <hickwu@qq.com>
2013-10-10 10:38:47 +0800

    + 通过 helper 来实现根据模版 title 处理实际输出的 title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # 根据不同情况返回 title
+  def full_title(page_title)
+    base_title = "Hick"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
